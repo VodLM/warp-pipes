c38c03fd202159ba3cb29c7a3af405e50e265865
Valentin Lievin <valentin.lievin@gmail.com>
2022-10-21 15:05:55 +0200

    SearchResult class

---
+++ warp_pipes/support/search_engines/__init__.py
@@ -0,0 +1,9 @@
+from .search_result import SearchResult
+
+# from .auto import AutoEngine
+# from .base import IndexEngine
+# from .es import ElasticSearchEngine
+# from .faiss import FaissEngine
+# from .token_faiss import TokenFaissEngine
+# from .topk import TopKEngine
+# from .document_lookup import DocumentLookupEngine
