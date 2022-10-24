3fcaf2d0df5614b2b9b74bf74a73bbe2eab30609
Valentin Liévin <valentin.lievin@gmail.com>
2022-10-24 15:32:50 +0200

    dependencies

--- examples/custom_search_engine.py
+++ examples/custom_search_engine.py
@@ -1,50 +1,28 @@
 from __future__ import annotations
 
-import abc
-import json
-from copy import copy
-from pathlib import Path
-from tempfile import tempdir
-import tempfile
 import shutil
-from typing import Any
-from typing import Dict
+import string
+import tempfile
+from pathlib import Path
 from typing import List
 from typing import Optional, Tuple
 
-from os import PathLike
+import datasets
 import numpy as np
-import omegaconf
+import rich
 import torch
 from datasets import Dataset
-from hydra.utils import instantiate
-from loguru import logger
-from omegaconf import DictConfig
-from omegaconf import ListConfig
 
-from warp_pipes.support.search_engines.search_result import SearchResult
-from warp_pipes.support.functional import camel_to_snake
-from warp_pipes.support.functional import get_batch_eg
-from warp_pipes.core.pipe import Pipe
 from warp_pipes.support.datastruct import Batch
-from warp_pipes.support.tensor_handler import TensorFormat, TensorLike, TensorHandler
-from warp_pipes.support.fingerprint import get_fingerprint
-from warp_pipes.support.shapes import infer_batch_size
-from warp_pipes.support.pretty import pprint_batch
-
-from random import random
 from warp_pipes.support.search_engines.base import SearchEngine
-import numpy as np
-import string
-import datasets
-import rich
-from pathlib import Path
+from warp_pipes.support.search_engines.search_result import SearchResult
+from warp_pipes.support.tensor_handler import TensorFormat, TensorLike, TensorHandler
 
 
 class CustomEngine(SearchEngine):
 
     def _build(self, vectors: Optional[TensorLike] = None,
-        corpus: Optional[Dataset] = None, **kwargs):
+               corpus: Optional[Dataset] = None, **kwargs):
         """build the index from the vectors or text."""
         self.vectors = TensorHandler(TensorFormat.TORCH)(vectors)
 
@@ -80,7 +58,7 @@ class CustomEngine(SearchEngine):
         return sim.gather(1, index=indices), indices
 
     def _search_chunk(
-        self, query: Batch, *, k: int, vectors: Optional[torch.Tensor], **kwargs
+            self, query: Batch, *, k: int, vectors: Optional[torch.Tensor], **kwargs
     ) -> SearchResult:
         """Search the index for the given query."""
         scores, indices = self.search(vectors, k=k)
@@ -90,13 +68,13 @@ class CustomEngine(SearchEngine):
 def run(cache_dir):
     # make dataset with random strings and random vectors
     vectors = np.random.randn(100, 8).astype(np.float32)
-    data = ["".join(np.random.choice(list(string.ascii_letters), 10).tolist() ) for _ in range(100)]
+    data = ["".join(np.random.choice(list(string.ascii_letters), 10).tolist()) for _ in range(100)]
     dataset = datasets.Dataset.from_dict({"text": data})
 
     path = Path(cache_dir)
     if path.exists():
         shutil.rmtree(path)
-    engine = CustomEngine(config={'path':path, 'k':3})
+    engine = CustomEngine(config={'path': path, 'k': 3})
     engine.build(vectors=vectors, corpus=dataset)
 
     # `search` interface
@@ -109,7 +87,6 @@ def run(cache_dir):
     output = engine({"query": ["shs", "kkj"]}, vectors=query_vector, k=3)
     rich.print(f"=== pipe.__call__() ===")
     rich.print(output)
-
 
 
 if __name__ == "__main__":
